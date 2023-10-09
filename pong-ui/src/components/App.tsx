ce26f54bf667ded7932a2d4e675bb35d90887dd9
Lassi Knuuttila <erkkipentti123@gmail.com>
2023-10-09 09:02:39 +0000

    WIP: Set up server (server is unresponsive atm)

--- pong-ui/src/components/App.tsx
+++ pong-ui/src/components/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-    return <h1>Nearly finished!</h1>;
+    const [message, setMessage] = useState('Loading...');
+    useEffect(() => {
+        const fetchMessage = async () => {
+            const response = await fetch(`http://localhost:${import.meta.env.PUBLIC_BACKEND_PORT}`)
+            console.log(response);
+            
+            setMessage(await response.text());
+        }
+        fetchMessage();
+    }, [message]);
+    
+    return <h1>Server says: {message}</h1>;
 }
 
 export default App;
