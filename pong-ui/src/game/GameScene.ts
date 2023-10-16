1fb36c505fd1516548a49f3b6e1139c14dd3a713
Aleksi <aleksi.lallukka@aalto.fi>
2023-10-16 17:47:57 +0300

    Implement physics engine into master

--- pong-ui/src/game/GameScene.ts
+++ pong-ui/src/game/GameScene.ts
@@ -18,10 +18,11 @@ export default class GameScene extends Phaser.Scene {
     this.load.setBaseURL("https://labs.phaser.io")
 
     this.load.image("wall", "https://i.imgur.com/WQUKFVC.png")
+    this.load.image("wall2", "https://i.imgur.com/YD8kW9f.png")
     this.load.image("ball", "https://i.imgur.com/xtFdsIU.png")
 
-    this.velocity = 600
-    this.ballVelocity = 600
+    this.velocity = 800
+    this.ballVelocity = 400
 
     this.backendUrl = `http://localhost:${import.meta.env.PUBLIC_BACKEND_PORT}`
     this.players = []
@@ -45,9 +46,12 @@ export default class GameScene extends Phaser.Scene {
     this.ball = this.add.sprite(400, 300, "ball")
     this.physics.world.enable(this.ball)
     this.ball.body!.velocity.set(this.ballVelocity, 0)
-    this.ball.onRightPaddle = false
     this.ball.body!.setBounce(1)
     this.ball.body!.collideWorldBounds = true
+    this.ball.onPlayerOnePaddle = false
+    this.ball.onPlayerTwoPaddle = false
+    this.ball.onPlayerThreePaddle = false
+    this.ball.onPlayerFourPaddle = false
   }
 
   update() {
@@ -55,16 +59,35 @@ export default class GameScene extends Phaser.Scene {
     this.players.forEach((player, i) => {
       this.movePaddle(i as keyof typeof playerConfig)
     })
+    this.ballCollision()
+
+    if (this.ball.onPlayerOnePaddle) {
+      this.increaseBallSpeed()
+      this.calculateXCollisions(this.players[0], 1)
+    }
+    if (this.ball.onPlayerTwoPaddle) {
+      this.increaseBallSpeed()
+      this.calculateYCollisions(this.players[1], 1)
+    }
+    if (this.ball.onPlayerThreePaddle) {
+      this.increaseBallSpeed()
+      this.calculateXCollisions(this.players[2], -1)
+    }
+    if (this.ball.onPlayerFourPaddle) {
+      this.increaseBallSpeed()
+      this.calculateYCollisions(this.players[3], -1)
+    }
   }
 
   addplayer(playerNo: keyof typeof playerConfig) {
     const config = playerConfig[playerNo]
-
-    const player: any = this.add.sprite(config.spawn.x, config.spawn.y, "wall")
+    const player: any =
+      config.direction === "y"
+        ? this.add.sprite(config.spawn.x, config.spawn.y, "wall")
+        : this.add.sprite(config.spawn.x, config.spawn.y, "wall2")
     player.setOrigin(0.5, 0.5)
-    player.scaleY = 0.33
+    config.direction === "y" ? (player.scaleY = 0.33) : (player.scaleX = 0.33)
     this.physics.world.enable(player)
-    player.angle = config.direction === "x" ? 90 : 0
     player.body!.collideWorldBounds = true
     player.body!.immovable = true
 
@@ -107,6 +130,86 @@ export default class GameScene extends Phaser.Scene {
       } else {
         player.body.velocity.y = 0
       }
+    }
+  }
+
+  //Function to check paddle and ball collisions
+  ballCollision() {
+    this.players.forEach((player, i) => {
+      i as keyof typeof playerConfig
+      if (i === 0) {
+        this.ball.onPlayerOnePaddle = false
+        this.physics.collide(this.players[0], this.ball, () => {
+          this.ball.onPlayerOnePaddle = true
+        })
+      }
+      if (i === 1) {
+        this.ball.onPlayerTwoPaddle = false
+        this.physics.collide(this.players[1], this.ball, () => {
+          this.ball.onPlayerTwoPaddle = true
+        })
+      }
+      if (i === 2) {
+        this.ball.onPlayerThreePaddle = false
+        this.physics.collide(this.players[2], this.ball, () => {
+          this.ball.onPlayerThreePaddle = true
+        })
+      }
+      if (i === 3) {
+        this.ball.onPlayerFourPaddle = false
+        this.physics.collide(this.players[3], this.ball, () => {
+          this.ball.onPlayerFourPaddle = true
+        })
+      }
+    })
+  }
+
+  calculateYCollisions(paddle: any, direction: any) {
+    //Calculate the relative collision point of the ball and paddle
+    const relativeIntersectY = paddle.y - this.ball.y
+    //Normalize the relativeIntersectY value
+    const normalizedRelativeIntersectionY =
+      relativeIntersectY / (paddle.displayHeight / 2)
+    //Calculate the bounce angle (maximum 45 degrees)
+    const bounceAngle = (normalizedRelativeIntersectionY * Math.PI) / 4
+    //Calculate the x-velocity of the ball
+    const ballVx = this.ballVelocity * Math.cos(bounceAngle)
+    //Calculate the y-velocity of the ball
+    const ballVy = this.ballVelocity * -Math.sin(bounceAngle)
+    //Update the x and y ball velocities
+
+    this.ball.body.velocity.y = ballVy
+    this.ball.body.velocity.x = direction * ballVx
+
+    direction === 1
+      ? (this.ball.onPlayerTwoPaddle = false)
+      : (this.ball.onPlayerFourPaddle = false)
+  }
+
+  calculateXCollisions(paddle: any, direction: any) {
+    //Calculate the relative collision point of the ball and paddle
+    const relativeIntersectX = paddle.x - this.ball.x
+    //Normalize the relativeIntersectX value
+    const normalizedRelativeIntersectionX =
+      relativeIntersectX / (paddle.displayWidth / 2)
+    //Calculate the bounce angle (maximum 45 degrees)
+    const bounceAngle = (normalizedRelativeIntersectionX * Math.PI) / 4
+    //Calculate the y-velocity of the ball
+    const ballVy = this.ballVelocity * Math.cos(bounceAngle)
+    //Calculate the x-velocity of the ball
+    const ballVx = this.ballVelocity * -Math.sin(bounceAngle)
+    //Update the x and y ball velocities
+    this.ball.body.velocity.y = direction * ballVy
+    this.ball.body.velocity.x = ballVx
+
+    direction === 1
+      ? (this.ball.onPlayerOnePaddle = false)
+      : (this.ball.onPlayerThreePaddle = false)
+  }
+  //Update ball velocity after each collision
+  increaseBallSpeed() {
+    if (this.ballVelocity < 1200) {
+      this.ballVelocity += 100
     }
   }
 }
